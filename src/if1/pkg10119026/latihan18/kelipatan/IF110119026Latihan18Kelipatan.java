c192e4ac3eae2a06d02b9973d47d3ec333b9e3eb
mkhatami34 <koabarab361@gmail.com>
2020-11-01 04:01:19 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan18/kelipatan/IF110119026Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan18.kelipatan;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Muhammad Khatami
+ * KELAS    : PBO
+ * NIM      : 10119026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan program
+ *                      kelipatan
+ */
+public class IF110119026Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double kelipatan = 0;
+        
+        //looping
+        for(int i = 1; i <= 10; i++){
+            kelipatan += 3.5;
+            System.out.println(kelipatan);
+        }
+
+    }
+    
+}
